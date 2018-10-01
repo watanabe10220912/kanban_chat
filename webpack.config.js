e9e2b0d4d2804b3f55b46f35d4328aec31f15017
ymitz <ymitz32@gmail.com>
2018-10-01 14:01:34 +0900

    enable watch files

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,55 @@ const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 
 
-module.exports = new Promise((resolve, reject) => {
-    let conf = {
-        entry: './src/index.js',
-        output: {
-            path: `${__dirname}/dist/js`,
-            filename: 'bundle.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    use: [
-                        {
-                            loader: "vue-loader"
-                        }
-                    ]
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        },
-                    ]
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader'
-                    ]
-                }
-            ]
-        },
-        resolve: {
-            modules: [path.join(__dirname, 'src'), 'node_modules'],
-            extensions: ['.js', '.vue','.json'],
-            alias: {
-                'vue$': 'vue/dist/vue.esm.js'
+module.exports = () => {
+    return new Promise((resolve, reject) => {
+        let conf = {
+            entry: './src/index.js',
+            output: {
+                path: `${__dirname}/dist/js`,
+                filename: 'bundle.js'
             },
-        },
-        plugins: [
-            new VueLoaderPlugin(),
-        ],
-    }
-    resolve(conf);
-});
+            module: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        use: [
+                            {
+                                loader: "vue-loader"
+                            }
+                        ]
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: [
+                            {
+                                loader: 'babel-loader',
+                            },
+                        ]
+                    },
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader'
+                        ]
+                    }
+                ]
+            },
+            resolve: {
+                modules: [path.join(__dirname, 'src'), 'node_modules'],
+                extensions: ['.js', '.vue', '.json'],
+                alias: {
+                    'vue$': 'vue/dist/vue.esm.js'
+                },
+            },
+            plugins: [
+                new VueLoaderPlugin(),
+            ],
+            devtool:'source-map',
+            mode:'development'
+        }
+        resolve(conf);
+
+})}
